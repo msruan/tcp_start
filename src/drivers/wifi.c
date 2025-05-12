faa69744a8b085a8ac4c378ca8514e9199071a5e
Ruan <santosruan2021@gmail.com>
2025-05-11 21:46:39 -0300

    fix: make wifi connection work

--- src/drivers/wifi.c
+++ src/drivers/wifi.c
@@ -13,22 +13,26 @@ void setup_wifi_on_sta_mode(const WifiConnectionConfig config)
     // show("SETUP: Iniciando Wifi", true);
     hard_assert(init_wifi_arch() == PICO_OK);
 
+    cyw43_arch_enable_sta_mode();
+
     cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, LED_ON);
-    
+
     hard_assert(connect_wifi(config) == PICO_OK);
     show("OK", true);
 
-
     printf("SETUP: Conexão WiFi bem sucedida! ");
-    printf("IP atual é %s", get_current_ip());
-    show(get_current_ip(), true);
+    // printf("IP atual é %s", get_current_ip());
+    // show(get_current_ip(), true);
+
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, LED_OFF);
+
 }
 
 int init_wifi_arch()
 {
     // cyw43_arch_deinit(); // Desativa o Wi-Fi
-
-    if (cyw43_arch_init() != PICO_OK) // EM produção, talvez seja melhor usar com with_country
+    while (cyw43_arch_init())
+    // if (cyw43_arch_init() != PICO_OK) // EM produção, talvez seja melhor usar com with_country
     {
         printf("SETUP: Falha ao inicializar Wi-Fi\n");
         sleep_ms(100);
@@ -43,11 +47,17 @@ int init_wifi_arch()
 int connect_wifi(const WifiConnectionConfig config)
 {
     printf("SETUP: Conectando à rede WiFi %s usando a senha %s!", config.SSID, config.password);
-    return cyw43_arch_wifi_connect_timeout_ms(
+    while (cyw43_arch_wifi_connect_timeout_ms(
         config.SSID,
         config.password,
         WIFI_AUTHORIZATION_TYPE,
-        WIFI_CONNECTION_TIMEOUT);
+        WIFI_CONNECTION_TIMEOUT))
+    {
+        printf("Falha ao conectar ao Wi-Fi\n");
+        sleep_ms(100);
+        return PICO_ERROR_GENERIC;
+    };
+    return PICO_OK;
 }
 
 void shutdown_wifi()
@@ -56,8 +66,10 @@ void shutdown_wifi()
     printf("TEARDOWN: WiFi desligado!");
 }
 
-char* get_current_ip(){
-    if(netif_default){
+char *get_current_ip()
+{
+    if (netif_default)
+    {
         return ipaddr_ntoa(&netif_default->ip_addr);
     }
     return NULL;
